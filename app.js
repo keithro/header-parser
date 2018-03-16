e748b7468f45c0545c0c08eb0d5c694b980ae555
Keith Rodriguez <rodriguez.keith@gmail.com>
2018-03-16 00:39:38 -0400

    Add minimal styling to index route

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
 const express = require('express');
 const { parser } = require('./utils/parser')
+const path = require('path');
 
 app = express();
 
 // Public Path
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes
 app.get('/', (req, res) => {
   res.sendFile('views/index.html', { root: __dirname });
 });
 
 app.get('/api', (req, res) => {
-  console.log(req.headers);
-
   const parsedData = parser(req);
   
-  res.send(parsedData);
+  res.status(200).send(parsedData);
 });
 
+app.get('/*', (req, res) => {
+  res.status(404);
+  res.redirect('/');
+})
+
+// Server
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   `Server running on port ${port}...`;
