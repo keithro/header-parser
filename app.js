0c8ff0dae357d0d7fa067f0e169bf7a12815b07d
Keith Rodriguez <rodriguez.keith@gmail.com>
2018-03-15 20:56:45 -0400

    Added routes and returning appropriate response data

--- app.js
+++ app.js
@@ -3,14 +3,25 @@ const express = require('express');
 app = express();
 
 // Public Path
-app.use(express.static(path.join(__dirname, 'public'));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.sendFile('views/index.html', { root: __dirname });
 });
 
 app.get('/api', (req, res) => {
-  res.send("hello from the query page");
+  const ipaddress = req.ip;
+  const language = req.get('Accept-Language').split(',')[0];
+  const userAgentString = req.get('User-Agent');
+  const start = userAgentString.indexOf('(');
+  const end = userAgentString.indexOf(')');
+  const software = userAgentString.slice(start + 1, end);
+  
+  res.send({
+    ipaddress,
+    language,
+    software
+  });
 });
 
 const port = process.env.PORT || 5000;
